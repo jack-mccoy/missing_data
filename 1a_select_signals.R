0c791a29a367312634e73e51f754d71a13cb8f55
McCoy <jpm2223@researchgrid.gsb.columbia.edu>
2022-06-22 16:05:57 -0400

    fixed errors in signal selection script

--- 1a_select_signals.R
+++ 1a_select_signals.R
@@ -54,23 +54,27 @@ signals <- fread("../data/signed_predictors_dl_wide.csv")
 setnames(signals, colnames(signals), tolower(colnames(signals)))
 
 # Ease of use
-signals[, yyyymm := as.yearmon(as.Date(as.character(yyyymm*10 + 1), "%Y%m%d"))]
+signals[, ":="(
+    yrmon = as.yearmon(as.Date(as.character(yyyymm*10 + 1), "%Y%m%d")),
+    yr = as.integer(substr(yyyymm, 1, 4)),
+    mon = as.integer(substr(yyyymm, 5, 6))
+)]
 
 crsp_data <- fread("../data/crsp_data.csv")[, .SD, .SDcols = !c("ret", "me")]
-crsp_data[, yyyymm := as.yearmon(yyyymm)]
+crsp_data[, yrmon := as.yearmon(yyyymm)]
 
 #==============================================================================#
 # Ensure we have all good variables ----
 #==============================================================================#
 
-signals <- merge(signals, crsp_data, by = c("permno", "yyyymm"))
+signals <- merge(signals, crsp_data[, !c("yyyymm")], by = c("permno", "yrmon"))
 
 # Only want to use variables that we can work with in each month
 # (remove binary signals in an overly-general way)
 bad_cols <- as.character(melt(signals[ # Counts for each month
-        opt$sample_start_year <= yyyymm & yyyymm <= opt$sample_end_year,
+        opt$sample_start_year <= yr & yr <= opt$sample_end_year,
         lapply(.SD, function(x) length(unique(x[!is.na(x)]))),
-        .SDcols = !c("permno", "yyyymm"),
+        .SDcols = !c("permno", "yyyymm", "yrmon", "yr", "mon"),
         by = yyyymm
     ][, # Count number of months with leq two unique observations 
         lapply(.SD, function(x) sum(x <= 2))
@@ -82,13 +86,13 @@ bad_cols <- as.character(melt(signals[ # Counts for each month
 # Filter to month specified
 # We do this AFTER making sure there are no bad months or it'll cause problems later on
 signals_selection_month <- signals[
-    opt$selection_year == year(yyyymm) &
-    opt$selection_month == month(yyyymm)
+    opt$selection_year == yr &
+    opt$selection_month == mon
 ]
 
 # Matrix of counts of observations, selection month
 counts <- t(signals_selection_month[, lapply(.SD, function(x) sum(!is.na(x))),
-    .SDcols = !c("permno", "yyyymm", bad_cols)])
+    .SDcols = !c("permno", "yyyymm", "yrmon", "yr", "mon", bad_cols)])
 
 # Get first N in descending order of counts
 signal_names <- rownames(counts)[order(-counts)][1:opt$n_signals]
