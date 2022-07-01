d0a7643035d2e621b16e9e0b31124f2f92eaa24f
Andrew Y. Chen <65506481+chenandrewy@users.noreply.github.com>
2022-07-01 16:53:48 -0400

    download_crsp: added CZ port download.

--- 0_download_crsp.R
+++ 0_download_crsp.R
@@ -7,6 +7,8 @@ library(data.table)
 library(getPass)
 library(RPostgres)
 library(zoo)
+library(googledrive)
+library(tidyverse)
 
 #==============================================================================#
 # Download CRSP ====
@@ -18,11 +20,11 @@ wrds_user <- getPass::getPass("WRDS username: ")
 wrds_pass <- getPass::getPass("WRDS pass: ")
 
 wrds <- DBI::dbConnect(RPostgres::Postgres(),
-    host = "wrds-pgdata.wharton.upenn.edu",
-    db = "wrds",
-    port = 9737,
-    user = wrds_user, 
-    pass = wrds_pass)
+                       host = "wrds-pgdata.wharton.upenn.edu",
+                       db = "wrds",
+                       port = 9737,
+                       user = wrds_user, 
+                       pass = wrds_pass)
 
 crspm <- as.data.table(DBI::dbGetQuery(wrds, "
     SELECT a.permno, a.date, a.ret, a.shrout, a.prc, 
@@ -43,37 +45,37 @@ DBI::dbDisconnect(wrds)
 ## Delisting returns ----
 
 crspm[, # Adapted from Andrew's github code
-    dlret := dplyr::case_when(
+      dlret := dplyr::case_when(
         is.na(dlret) 
-            & (dlstcd == 500 | (520 <= dlstcd & dlstcd <= 584))
-            & (exchcd == 1 | exchcd == 2) 
+        & (dlstcd == 500 | (520 <= dlstcd & dlstcd <= 584))
+        & (exchcd == 1 | exchcd == 2) 
         ~ -0.25,
         is.na(dlret)
-            & (dlstcd == 500 | (dlstcd >=520 & dlstcd <=584))
-            & (exchcd == 3)
+        & (dlstcd == 500 | (dlstcd >=520 & dlstcd <=584))
+        & (exchcd == 3)
         ~ -0.55,
         dlret < -1 & !is.na(dlret) ~ -1,
         TRUE ~ 0
-    )
+      )
 ][ # return is the return plus delisting return
-    , ret := ret + dlret
+  , ret := ret + dlret
 ][ # in cases where no ret but non-zero dlret
-    is.na(ret) & dlret != 0,
-    ret := dlret
+  is.na(ret) & dlret != 0,
+  ret := dlret
 ]
 
 ## Lastly, the signals that aren't in Andrew's master file ----
 # Signals are SIGNED (higher signal ==> higher return)
 
 crsp_final <- crspm[, .(
-    permno,
-    yyyymm = as.yearmon(as.Date(date)),
-    ret = 100 * ret,
-    me = abs(prc) * shrout,
-    price = -1 * log(abs(prc))
+  permno,
+  yyyymm = as.yearmon(as.Date(date)),
+  ret = 100 * ret,
+  me = abs(prc) * shrout,
+  price = -1 * log(abs(prc))
 )][, ":="(
-    streversal = -1 * ifelse(is.na(ret), 0, ret),
-    size = ifelse(me > 0, -1 * log(me), NA)
+  streversal = -1 * ifelse(is.na(ret), 0, ret),
+  size = ifelse(me > 0, -1 * log(me), NA)
 )]
 
 # Memory
@@ -111,3 +113,14 @@ dl = drive_download(target_dribble, path = '../data/deleteme.zip', overwrite = T
 # unzip, clean up
 unzip('../data/deleteme.zip', exdir = '../data')
 file.remove('../data/deleteme.zip')
+
+
+## download ports ====
+# this is just for counting and making signal lists
+target_dribble = pathRelease %>% drive_ls() %>% 
+  filter(name == 'Portfolios') %>% drive_ls() %>% 
+  filter(name == 'Full Sets OP') %>% drive_ls() %>% 
+  filter(name == 'PredictorPortsFull.csv')
+
+drive_download(target_dribble, path = '../data/PredictorPortsFull.csv', overwrite = T)
+
