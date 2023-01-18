b4c93e0ab3fb5ffdbb9113be9a30ada027994534
Jack McCoy <jpm2223@columbia.edu>
2023-01-18 11:31:30 -0500

    3_run_pcr.sh now working for linux grid

--- 3_run_pcr.sh
+++ 3_run_pcr.sh
@@ -2,11 +2,11 @@
 
 # Parameters to control the jobs
 start_yr=1995
-end_yr=1996
+end_yr=2020
 main_path="../output/"
 
 # Parameters for principal component regressions
-signals_file="$main_path/signals_best125_1985.txt" # file with list of signals to use
+signals_file="${main_path}signals_best125_1985.txt" # file with list of signals to use
 n_pcs=75 # number of PCs in maximal regression
 n_years=10 # number of years for principal components/predictive regs
 quantile_prob=0.1 # quantile to form long/short portfolios (0.1 means deciles)
@@ -15,12 +15,12 @@ quantile_prob=0.1 # quantile to form long/short portfolios (0.1 means deciles)
 sample_start_yr=$(($start_yr-$n_years))
 
 # Paths
-params_path="$main_path/impute_ests/" 
+params_path="${main_path}impute_ests/" 
 
 # Big datasets
-tmp_file_mn="$main_path/bcsignals_non.csv"
-tmp_file_em="$main_path/bcsignals_em.csv"
-tmp_file_ac="$main_path/bcsignals_availcase.csv"
+tmp_file_mn="${main_path}/bcsignals/bcsignals_none.csv"
+tmp_file_em="${main_path}/bcsignals/bcsignals_em.csv"
+tmp_file_ac="${main_path}/bcsignals/bcsignals_availcase.csv"
 
 # Submit the data prepping job
 out_mn=$(Rscript --grid_submit=batch \
@@ -28,36 +28,36 @@ out_mn=$(Rscript --grid_submit=batch \
     --grid_mem=200G \
     --grid_email="jmccoy26@gsb.columbia.edu" \
     3a_prep_big_data.R \
-        --impute_type="none"
+        --impute_type="none" \
         --impute_vec=$signals_file \
         --sample_start_year=$sample_start_yr \
         --sample_end_year=$end_yr \
         --params_path=$params_path \
-        --bcsignals_filename=$tmp_file_mn
+        --bcsignals_filename=$tmp_file_mn)
 
 out_em=$(Rscript --grid_submit=batch \
     --grid_ncpus=12 \
     --grid_mem=200G \
     --grid_email="jmccoy26@gsb.columbia.edu" \
     3a_prep_big_data.R \
-        --impute_type="em"
+        --impute_type="em" \
         --impute_vec=$signals_file \
         --sample_start_year=$sample_start_yr \
         --sample_end_year=$end_yr \
         --params_path=$params_path \
-        --bcsignals_filename=$tmp_file_em
+        --bcsignals_filename=$tmp_file_em)
 
 out_ac=$(Rscript --grid_submit=batch \
     --grid_ncpus=12 \
     --grid_mem=200G \
     --grid_email="jmccoy26@gsb.columbia.edu" \
     3a_prep_big_data.R \
-        --impute_type="availcase"
+        --impute_type="availcase" \
         --impute_vec=$signals_file \
         --sample_start_year=$sample_start_yr \
         --sample_end_year=$end_yr \
         --params_path=$params_path \
-        --bcsignals_filename=$tmp_file_ac
+        --bcsignals_filename=$tmp_file_ac)
 
 # Extract the job ID for monitoring
 jobid_mn=$( echo $out_mn | grep -o -E '[0-9]+' )
@@ -67,7 +67,7 @@ jobid_ac=$( echo $out_ac | grep -o -E '[0-9]+' )
 # Run the PCR with the new data
 # These take up so much memory that I need to do one year at a time
 # But they each run relatively quickly
-for _yr in ` $start_yr $end_yr `; do
+for _yr in $(eval echo "{$start_yr..$end_yr}"); do
     for forecast in "pca" "spca1" "spca2"; do 
         
         if [ $forecast = "pca" ]; then
