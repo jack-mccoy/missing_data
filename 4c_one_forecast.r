306233b034234eccd0ff06f533faf2f4c242940e
Andrew <ayc1678@gmail.com>
2023-02-09 20:22:25 -0500

    decent draft of 4c_one_forecast.r

--- 4c_one_forecast.r
+++ 4c_one_forecast.r
@@ -11,14 +11,17 @@ library(ggplot2)
 library(zoo)
 library(janitor)
 
+library(tensorflow)
+library(keras)
+
 opt = list(
   data_file = '../output/bcsignals/bcsignals_none.csv'
   , yearm_begin = '1995-06'
   , refit_period = 12 # months
-  , train_months = 12*10 # months
+  , insamp_months = 12*10 # months
   , yearm_end = '2020-12'
   , cores_frac = 0.5
-  , model = 'ranger' # 'ranger' or 'lm'
+  , model = 'keras' # 'ranger' or 'lm' or 'keras'
 )
 
 # output names
@@ -26,13 +29,17 @@ customname = NULL
 outroot = '../output/forecast/'
 
 # settings for specific algos
-optranger = list(
-  num.tree = 300, max.depth = 3, mtry = 100/3
+# max.depth = NULL dramatically slows things down
+optranger = list( 
+  num.tree = 500, max.depth = 3, mtry = 30
+)
+
+optkeras = list(
+  lambda1 = 1e-3, epochs = 100, batch_size = 10000, learning_rate = 0.001, patience = 5
 )
 
 # keep this subset for testing, use NULL for keep all
-signals_keep = NULL # e.g. c('size','bm','mom12m') 
-
+signals_keep = NULL # e.g. c('size','bm','mom12m')
 
 # auto setup
 if (is.null(customname)) {
@@ -100,77 +107,110 @@ dat <- dat[complete.cases(
 )]
 
 
-#==============================================================================# 
-# Loop over forecast periods ----
-#==============================================================================#
 
-# create list of forecast periods
-forecast_list = data.frame(
-  train_end = seq(as.yearmon(opt$yearm_begin), as.yearmon(opt$yearm_end), opt$refit_period/12)
-) %>% 
-  mutate(
-    train_begin = train_end - opt$train_months/12
-    , test_begin = train_end + 1/12
-    , test_end = lead(train_end) - 1/12
-  ) %>% 
-  filter(row_number() < n()) # don't test after opt$yearm_end
+
+
+#==============================================================================# 
+# fit_and_forecast function ----
+#==============================================================================#
 
 # function for choosing forecast
 fit_and_forecast = function(modelname){
   if (modelname == 'lm'){
     form = paste0('bh1m ~ ', paste(signals_list, collapse = '+'))  
-    fit = lm(form, data=train)
-    Ebh1m = predict(fit, test)
+    fit = lm(form, data=insamp)
+    Ebh1m = predict(fit, oos)
     
   } else if (modelname == 'ranger'){
-    form = paste0('bh1m ~ ', paste(signals_list, collapse = '+'))  
-    fit = ranger(form
-                 , data=train
+    
+    fit = ranger(x = as.matrix(insamp %>% select(all_of(signals_list)))
+                 , y = as.matrix(insamp %>% select(bh1m))
                  , num.trees = optranger$num.tree
                  , num.threads = floor(parallel::detectCores()*opt$cores_frac)
                  , max.depth = optranger$max.depth
                  , mtry = optranger$mtry
-                 )
-    pred.ra = predict(fit, test) # annoying middle step for ranger
+    )
+    
+    pred.ra = predict(fit, oos) 
     Ebh1m = pred.ra$predictions
-  } 
+    
+  } else if (modelname == 'keras') {
+    
+    # create norm layer, fit the state of layer to data
+    #   seems to be necessary, or something like it that uses adapt
+    normalizer <- layer_normalization(axis = -1L)
+    normalizer %>% adapt(as.matrix(insamp %>% select(all_of(signals_list))))
+    
+    # build model
+    keras_model = keras_model_sequential() %>% 
+      normalizer() %>% 
+      layer_dense(32, activation = 'relu'
+                  , kernel_regularizer = regularizer_l1(optkeras$lambda1)) %>%
+      # layer_dense(64, activation = 'relu') %>%
+      layer_dense(1)
+    
+    keras_model %>%  compile(
+      loss = 'mean_squared_error',
+      optimizer = optimizer_adam(
+        learning_rate = optkeras$learning_rate
+      )
+    )
+    
+    # fit
+    # ref: https://tensorflow.rstudio.com/guides/keras/basics.html#callbacks
+    history <- keras_model %>% fit(
+      as.matrix(insamp %>% select(all_of(signals_list))),
+      as.matrix(insamp %>% select(bh1m)),
+      verbose = 1,
+      epochs = optkeras$epochs,
+      batch_size = optkeras$batch_size,
+      callbacks = callback_early_stopping(patience = optkeras$patience)
+    )
+    
+    Ebh1m = predict(keras_model, as.matrix(oos %>% select(all_of(signals_list))))
+    
+  }
+  
+  
   return(Ebh1m)
 }  
+
+
+
+#==============================================================================# 
+# Loop over sample periods ----
+#==============================================================================#
+
+# create list of forecast periods
+sample_list = data.frame(
+  insamp_end = seq(as.yearmon(opt$yearm_begin), as.yearmon(opt$yearm_end), opt$refit_period/12)
+) %>% 
+  mutate(
+    insamp_begin = insamp_end - opt$insamp_months/12
+    , oos_begin = insamp_end + 1/12
+    , oos_end = lead(insamp_end) - 1/12
+  ) %>% 
+  filter(row_number() < n()) # don't oos after opt$yearm_end
 
 
 # loop
 tic = Sys.time()
 outlist = list()
-file.remove(paste0(outroot,outfolder,'/current_forecast.log'))
-for (fi in 1:dim(forecast_list)[1]){
-  fcur = forecast_list[fi, ]
+file.remove(paste0(outroot,outfolder,'/forecast-loop.log'))
+for (fi in 1:dim(sample_list)[1]){
+  fcur = sample_list[fi, ]
   
-  print(paste0('forecasting in ', fcur$train_end))  
+  print(paste0('forecasting in ', fcur$insamp_end))  
   
   # create subsamples
-  train = dat[yyyymm >= fcur$train_begin & yyyymm <= fcur$train_end]
-  test  = dat[yyyymm >= fcur$test_begin & yyyymm <= fcur$test_end]
+  insamp = dat[yyyymm >= fcur$insamp_begin & yyyymm <= fcur$insamp_end]
+  oos  = dat[yyyymm >= fcur$oos_begin & yyyymm <= fcur$oos_end]
   
   # fit and forecast
   Ebh1m = fit_and_forecast(opt$model)
   
-  # if (opt$model == 'lm'){
-  #   form = paste0('bh1m ~ ', paste(signals_list, collapse = '+'))  
-  #   fit = lm(form, data=train)
-  #   Ebh1m = predict(fit, test)
-  #   
-  # } else if (opt$model == 'ranger'){
-  #   form = paste0('bh1m ~ ', paste(signals_list, collapse = '+'))  
-  #   fit = ranger(form
-  #                , data=train
-  #                , num.trees = optranger$num.tree
-  #                , num.threads = floor(parallel::detectCores()*opt$cores_frac))
-  #   pred.ra = predict(fit, test) # annoying middle step for ranger
-  #   Ebh1m = pred.ra$predictions
-  # }
-  
   # save
-  outlist[[fi]] = test[,.(permno,yyyymm)][
+  outlist[[fi]] = oos[,.(permno,yyyymm)][
     , Ebh1m := Ebh1m
   ]
   
@@ -178,12 +218,12 @@ for (fi in 1:dim(forecast_list)[1]){
   min_elapsed = round(as.numeric(Sys.time() - tic, units = 'mins'), 1)
   log.text <- paste0(
     Sys.time()
-    , " train_end = ", fcur$train_end
+    , " insamp_end = ", fcur$insamp_end
     , " min elapsed = ", min_elapsed
     , " min per fit = ", round(min_elapsed / fi, 2)
-    , " min remaining = ", round(min_elapsed / fi * (dim(forecast_list)[1]-fi+1), 1)
+    , " min remaining = ", round(min_elapsed / fi * (dim(sample_list)[1]-fi+1), 1)
   )
-  write.table(log.text, paste0(outroot,outfolder, '/current_forecast.log')
+  write.table(log.text, paste0(outroot,outfolder, '/forecast-loop.log')
               , append = TRUE, row.names = FALSE, col.names = FALSE)
 }
 
@@ -223,10 +263,6 @@ port = port %>% rbind(LSport)
 # Save to disk ----
 #==============================================================================#
 
-opt %>% t()
-
-
-
 
 # save forecasts
 fwrite(forecast, paste0(outroot,outfolder,'/permno-month-forecast.csv'))
@@ -258,7 +294,14 @@ format(optranger)
 cat('\n\n')
 
 print('================================')
+print('optkeras')
+format(optkeras)
+cat('\n\n')
+
+
+print('================================')
 print('signals_used')
 as.data.frame(signals_list)
 cat('\n\n')
+
 sink()
