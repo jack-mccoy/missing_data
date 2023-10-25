f97205b145bcbbc7897fb9a281f823cb1398fe04
Andrew Y. Chen <65506481+chenandrewy@users.noreply.github.com>
2023-10-24 23:07:42 -0400

    miss plot now by data category

--- 6a_miss_plot.R
+++ 6a_miss_plot.R
@@ -12,7 +12,7 @@ source("functions.R")
 # set file paths and choose yearmons
 getFilePaths()
 plot_path <- paste0(FILEPATHS$out_path, "plots/")
-dates <- as.yearmon(c("Jun 1985","Jun 1990", "Jun 2000", "Jun 2010"))
+dates <- as.yearmon(c("Jun 1985","Jun 1990", "Jun 2000", "Jun 2010", "Jun 2020"))
 
 dir.create(plot_path, showWarnings = F)
 
@@ -25,73 +25,6 @@ dat <- fread(paste0(FILEPATHS$data_path, "bcsignals/bcsignals_none.csv"))[
 ]
 dat[, yyyymm := as.yearmon(yyyymm)]
 
-#===============================================================================#
-# Output plots ----
-#===============================================================================#
-
-# Missingness plot function
-missPlot <- function(data, rhs_vars, xlab = "Stock i.d.", ylab = "Predictor i.d.",
-                     title = "Missingness Map"
-) {
-  data2 <- copy(as.data.table(data))
-  
-  nas <- is.na(data2[,..rhs_vars])
-  
-  sort_by_na <- do.call("order", as.data.table(is.na(data2)))
-  
-  nas <- as.data.table(nas)[sort_by_na]
-  
-  col_miss <- colSums(nas)
-  col_ind <- order(-col_miss) 
-  nas <- nas[, ..col_ind]
-  signal_id <- data.table(variable = colnames(nas), 
-                          id = factor(ncol(nas):1, levels = ncol(nas):1)) 
-  nas[, firm_id := 1:nrow(nas)]
-  
-  ratio <- nrow(nas)*1/(ncol(nas) - 1)
-  nas_long <- merge(melt(nas, id = "firm_id"), signal_id, by = "variable") 
-  
-  miss_plot <- ggplot(data = nas_long, aes(x = firm_id, y = id)) + 
-    geom_raster(aes(fill = value)) +
-    labs(x = xlab, y = ylab, color = "") + 
-    scale_fill_manual(
-      name = "",
-      labels = c("Observed","Missing"),
-      values = c('lightgrey', MATRED)
-    ) + 
-    theme_bw() +
-    theme(
-      text = element_text(size = 22),
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      panel.background = element_blank(),
-      legend.position = "top"
-    ) +
-    scale_x_continuous(expand = c(0,0), breaks = c(1,seq(1000,12000,1000))) +
-    scale_y_discrete(breaks = c(1, seq(0, ncol(nas), by = 25))) +
-    ggtitle(title)# + 
-  #coord_fixed(ratio)
-  
-  return(miss_plot)
-}
-
-signals_keep = names(dat)
-signals_keep = signals_keep[!signals_keep %in% c('permno','yyyymm')]
-
-for (dd in 1:length(dates)) {
-    date_file <- gsub(" ", "", dates[dd]) 
-    tmp <- missPlot(dat[yyyymm == dates[dd]], 
-        rhs_vars = signals_keep,
-        title = "")
-    ggsave(plot = tmp, filename = paste0(plot_path, "missplot_", date_file, ".pdf"), 
-        width = 10, height = 7, unit = "in")
-    rm(tmp)
-}
-
-#===============================================================================#
-# Testing plot by data category ----
-#===============================================================================#
-
 # import get data categories
 signaldoc = fread(paste0(FILEPATHS$data_path, "raw/SignalDoc.csv "))
 signaldoc[ , signalname := tolower(Acronym)]
@@ -103,73 +36,91 @@ signaldoc[ !Cat.Data %in% data_list, Cat.Data := 'Other']
 signaldoc[ , Cat.Data 
   := factor(Cat.Data, levels = c('Price','Accounting','Analyst','Trading','Other'))]
 
-# function inputs
+# other setup
 signals_keep = names(dat)
 signals_keep = signals_keep[!signals_keep %in% c('permno','yyyymm')]
+nsignal = length(signals_keep)
 
-data = dat[yyyymm == dates[1]]
-rhs_vars = signals_keep
-title = ''
-xlab = 'Stock i.d.'
-ylab = 'Predictor i.d.'
+#==============================================================================#
+# Plotting Function ----
+#===============================================================================#
 
-# function body sketch
-data2 <- copy(as.data.table(data))
-nas <- is.na(data2[,..rhs_vars])
+# function for outputting missplot pdf
+make_missplot_pdf = function(yearm_select){    
 
-# sort stocks by number of na's
-sort_by_na <- do.call("order", as.data.table(is.na(data2)))
-nas <- as.data.table(nas)[sort_by_na]
+  # create (permno,signalname,miss) long data
+  misslong = dat[yyyymm == as.yearmon(yearm_select)] %>% select(-yyyymm)  %>% 
+    pivot_longer(cols = -permno, names_to = 'signalname', values_to = 'value') %>% 
+    mutate(miss = is.na(value)) %>% 
+    left_join(signaldoc, by = 'signalname') %>% 
+    select(permno, signalname, miss, Cat.Data)     
 
-# sort predictors by data category and then by number of na's
-signalsort = merge(
-  data.table(signalname = colnames(nas), nas = colSums(nas)), 
-  signaldoc, by = 'signalname', all.x = T)
-signalsort[ , id_old := .I]
-setorder(signalsort, -Cat.Data, -nas)
-signalsort[ , id := factor(dim(signalsort)[1]:1, 
-  levels = dim(signalsort)[1]:1)]
+  # sort signals by data category and then by number of na's
+  signalsum = misslong %>% 
+    group_by(Cat.Data, signalname) %>% summarize(nmiss_signal = sum(miss)) %>% ungroup() %>% 
+    arrange(desc(Cat.Data),-nmiss_signal) %>% 
+    # mutate(signal_id = as.factor(row_number(), levels = nsignal:1)) 
+    mutate(signal_id = factor(nsignal-row_number()+1, levels = nsignal:1)) 
 
-nas = setcolorder(nas, signalsort$signalname)
-# signal_id <- data.table(variable = colnames(nas), 
-#                         id = factor(ncol(nas):1, levels = ncol(nas):1)) 
-nas[, firm_id := 1:nrow(nas)]
+  # sort stocks by data category and then by number of na's
+  stocksum = misslong %>%
+    group_by(permno) %>% summarize(nmiss_stock = sum(miss)) %>% ungroup() %>%
+    arrange(nmiss_stock) %>%
+    mutate(stock_id = row_number())
+      
+  # merge on sorted ids
+  misslong2 = misslong %>% 
+    left_join(signalsum %>% select(signalname,signal_id), by = 'signalname') %>%
+    left_join(stocksum %>% select(permno,stock_id), by = 'permno') 
 
-# prep for plotting
-nas_long <- merge(melt(nas, id = "firm_id"), signalsort[, .(signalname,Cat.Data,id)], 
-  by.x = "variable", by.y = "signalname") 
-y_data_change = nas_long %>% group_by(Cat.Data) %>% 
-  summarize(nsignal = n_distinct(id)) %>% 
-  mutate(y_pos = length(signals_keep) - cumsum(nsignal)+0.5) %>% 
-  pull(y_pos)
+  # find signal ids for when data category changes
+  y_data_change = misslong2 %>% 
+    mutate(nsignal = n_distinct(signal_id)) %>% 
+    group_by(Cat.Data) %>% 
+    summarize(nsignalcat = n_distinct(signal_id)) %>% 
+    mutate(y_pos = nsignal - cumsum(nsignalcat)+0.2)  %>% 
+    pull(y_pos)
 
-# plot
-color_datacat = 'black'
-miss_plot <- ggplot(data = nas_long, aes(x = firm_id, y = id)) + 
-  geom_raster(aes(fill = value)) +
-  labs(x = xlab, y = ylab, color = "") + 
-  scale_fill_manual(
-    name = "",
-    labels = c("Observed","Missing"),
-    values = c('lightgrey', MATRED)
-  ) + 
-  theme_bw() +
-  theme(
-    text = element_text(size = 22),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    legend.position = "top"
-  ) +
-  scale_x_continuous(expand = c(0,0), breaks = c(1,seq(1000,12000,1000))) +
-  scale_y_discrete(breaks = c(1, seq(0, ncol(nas), by = 25))) +
-  ggtitle(title) +
-  # horizontal lines for y_data_change
-  geom_hline(yintercept = y_data_change, color = color_datacat) +
-  annotate("text", x = 100, y = c(160, y_data_change[1:4])-5.5, 
-           label = data_list[seq(1,5,1)], 
-           color = color_datacat, size = 5, hjust = 0, vjust = 0) 
+  # plot
+  color_datacat = 'black'
+  miss_plot <- ggplot(data = misslong2, aes(x = stock_id, y = signal_id)) + 
+    geom_raster(aes(fill = miss)) +
+    labs(x = 'Stock i.d.', y = 'Predictor i.d.', color = "") + 
+    scale_fill_manual(
+      name = "",
+      labels = c("Observed","Missing"),
+      values = c('lightgrey', MATRED)
+    ) + 
+    theme_bw() +
+    theme(
+      text = element_text(size = 22),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      legend.position = "top",
+      plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")
+    ) +
+    scale_x_continuous(expand = c(0,0), breaks = c(1,seq(1000,12000,1000))) +
+    scale_y_discrete(breaks = c(1, seq(0, nsignal, by = 25))) +
+    # horizontal lines for y_data_change
+    geom_hline(yintercept = y_data_change, color = color_datacat) +
+    annotate("text", x = 100, y = c(160, y_data_change[1:4])-5.5, 
+            label = data_list[seq(1,5,1)], 
+            color = color_datacat, size = 5, hjust = 0, vjust = 0) 
 
-ggsave(miss_plot, 
-    filename = paste0(FILEPATHS$out_path, "plots/missplotcat_", data2$yyyymm[1], ".pdf"),
-    width = 10, height = 7, unit = "in")
+  ggsave(miss_plot, 
+      filename = paste0(FILEPATHS$out_path, "plots/missplotcat_", yearm_select, ".pdf"),
+      width = 10, height = 7, unit = "in")
+
+} # end function make_missplot_pdf
+
+#==============================================================================#
+# Run Function Several Times ----
+#===============================================================================#
+
+# make plot for each date
+for (dd in 1:length(dates)) {    
+    make_missplot_pdf(dates[dd])
+}
+
+print('Finished making missplots by data category')
