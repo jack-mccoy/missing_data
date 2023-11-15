e11c6d68ef0667a609da6338a2b813496737ce0c
Jack McCoy <jpm2223@columbia.edu>
2023-11-14 19:08:33 -0500

    changed FILEPATHS.R to generic unix/linux paths

--- FILEPATHS.R
+++ FILEPATHS.R
@@ -1,8 +1,5 @@
 FILEPATHS <- list(
-    data_path = "/scratch/jpm2223/missing_data/data_all_filtered_aug2023/",
-    out_path = "/scratch/jpm2223/missing_data/output_all_filtered_aug2023/",
-    signal_list = "/scratch/jpm2223/missing_data/data_all_filtered_aug2023/signals_all_1985.txt"
-    #data_path = "../data-2023-09/data/",
-    #out_path = "../data-2023-09/output/",
-    #signal_list = "../data-2023-09/data/signals_best160_1985.txt"
+    data_path = "../data_cm/",
+    out_path = "../output_cm/",
+    signal_list = "../data_cm/signals_all_1985.txt"
 )
